91c7caf8659e8d675a2aee6a6d262c07f2ab1fa3
Kimberly Aviles <01010110.v@gmail.com>
2019-03-20 04:12:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,17 +16,17 @@ end
 
 # class Student < User
 #   attr_accessor :knowledge
-# 
+#
 #   def initialize
 #     @knowledge = []
 #   end
-# 
+#
 #   def learn(new_knowlege_string)
 #     self.knowledge << new_knowlege_string
 #   end
-# 
+#
 #   def knowledge
 #     @knowledge
 #   end
-# 
+#
 # end
